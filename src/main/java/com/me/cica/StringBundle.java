82d2ff42d9263777581be91b5f355d54b2d04da1
Robert Bjarnason <robert.bjarnason@gmail.com>
2019-05-27 17:32:53 +0000

    Initial import from svc

---
+++ src/main/java/com/me/cica/StringBundle.java
@@ -0,0 +1,10 @@
+package com.me.cica;
+
+class StringBundle {
+    final static String WARC_VERSION = "WARC/1.0";
+    final static String REQUEST_MARKER = "WARC-Type: request";
+    final static String CONTENT_TYPE = "Content-Type";
+    final static String ENTITY_TYPE = "text/html";
+    final static String HTTP_HEADER_RESPONSE_OK = "HTTP/1.1 200 OK";
+    final static String HTTP_HEADER_HOST = "Host: ";
+}
